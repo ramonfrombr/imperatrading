6a3ad8b32af2dcdcc8221fbb287180e324c7e950
ramonfrombr <ramonfrombr@gmail.com>
2022-02-08 13:20:26 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+
 import React, { useState } from "react";
+
 import Inicio from "./paginas";
+
 import PaginaEntrar from "./paginas/entrar";
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+    
 	const [menuAberto, definirMenuAberto] = useState(false);
 
 	return (
